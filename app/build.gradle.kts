563076fbd76d6ef56f01fa7011d050daf8bc8610
Hugo Correia <hah_correia@hotmail.com>
2026-09-20 12:10:39 +0100

    Release 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "pt.haconnect.predit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.11.0"
+        versionCode = 15
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
